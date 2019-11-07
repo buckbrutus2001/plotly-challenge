cfc65bdb831b43ea867bd3d2297a2a5a1a6b68d6
Junghyun Son <shawnson512@gmail.com>
2019-11-06 23:35:23 -0600

    update

---
+++ static/js/app.js
@@ -0,0 +1,108 @@
+function buildMetadata(sample) {
+
+  // @TODO: Complete the following function that builds the metadata panel
+
+  // Use `d3.json` to fetch the metadata for a sample
+    d3.json(`/metadata/${sample}`).then((data) => {
+
+      // Use d3 to select the panel with id of `#sample-metadata`
+    
+        let panel = d3.select("#sample-metadata");
+
+      // Use `.html("") to clear any existing metadata
+        panel.html("");
+      // Use `Object.entries` to add each key and value pair to the panel
+      // Hint: Inside the loop, you will need to use d3 to append new
+      // tags for each key-value in the metadata.
+        Object.entries(data).forEach(([key, value]) => {
+          panel.append("h6").text(`${key}:${value} \n`);
+        })
+    })
+    // BONUS: Build the Gauge Chart
+    // buildGauge(data.WFREQ);
+}
+
+function buildCharts(sample) {
+
+  // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(`/samples/${sample}`).then((data) =>{
+    // @TODO: Build a Bubble Chart using the sample data
+    const sample_values = data.sample_values;
+    const otu_ids = data.otu_ids;
+    const otu_lables = data.otu_lables;
+
+    let bubble_trace = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_lables,
+      mode: "markers",
+      marker: {
+        color:otu_ids,
+        size: sample_values
+      }
+    }]
+
+    
+
+    let bubble_layout ={
+      title : "Belly Button Diversity Bubble Chart"
+    };
+
+    Plotly.newPlot('bubble', bubble_trace, bubble_layout);
+
+    // @TODO: Build a Pie Chart
+    // HINT: You will need to use slice() to grab the top 10 sample_values,
+    // otu_ids, and labels (10 each).
+
+
+    const values = data.sample_values.slice(0,10);
+    const labels = data.otu_ids.slice(0,10);
+    const hovertext = data.otu_labels.slice(0,10);
+
+    let pie_trace = [{
+        values: values,
+        labels: labels,
+        hovertext: hovertext,
+        type: 'pie'
+      }];
+
+    let pie_layout = {
+      title : "Belly Button Diversity Pie Chart"
+    };
+    
+   
+    Plotly.newPlot('pie', pie_trace, pie_layout);
+  
+    });
+
+  
+}
+
+function init() {
+  // Grab a reference to the dropdown select element
+  var selector = d3.select("#selDataset");
+
+  // Use the list of sample names to populate the select options
+  d3.json("/names").then((sampleNames) => {
+    sampleNames.forEach((sample) => {
+      selector
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+
+    // Use the first sample from the list to build the initial plots
+    const firstSample = sampleNames[0];
+    buildCharts(firstSample);
+    buildMetadata(firstSample);
+  });
+}
+
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
+}
+
+// Initialize the dashboard
+init();
